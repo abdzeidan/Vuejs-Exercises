33df3cb25746d5ab31bfcd9b1aa35a16f853c461
Abdullah Zeidan <zeid0012@algonquinlive.com>
2019-01-17 11:58:07 -0500

    initializing PlayGround with Seperation of Concerns

---
+++ src/components/PlayGround/PlayGround.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="hello"></div>
+</template>
+
+<script src = "./PlayGround.js"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./PlayGround.css" scoped></style>
